f321f835abff7962cd105d37e560eb1ccab98b5e
levkany.dev <levkany.dev@gmail.com>
2024-03-27 15:24:05 +0200

    removed server code

--- commands/system.py
+++ commands/system.py
@@ -1,24 +1,3 @@
-"""
-    Copyright (c) 2024 levkany
-    All rights reserved.
-
-    The source code, including any accompanying documentation
-    or files, is the exclusive property of levkany
-    ("Owner") and is confidential and proprietary.
-
-    No part of the source code may be reproduced, distributed,
-    or transmitted in any form or by any means, including photocopying,
-    recording, or other electronic or mechanical methods,
-    without the prior written permission of the Owner.
-
-    Unauthorized use, reproduction, or distribution of the source code
-    or any portion thereof is strictly prohibited and may result
-    in severe civil and criminal penalties.
-
-    For licensing inquiries, please contact levkany.dev@gmail.com
-"""
-
-
 from core.command import Command
 from core.command import CommandType
 
@@ -36,4 +15,9 @@ list_dir_hidden = Command(
 clear = Command(
     CommandType.READ,
     'clear'
+)
+
+total_cores = Command(
+    CommandType.READ,
+    'nproc --all'
 )
