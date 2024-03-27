f8ba6c4b122dc579f83d0d9c0e17c86f96d413ad
levkany.dev <levkany.dev@gmail.com>
2024-03-27 16:07:46 +0200

    added support for multiple commands execution at once

--- src/lindash_levkany/lindash.py
+++ src/lindash_levkany/lindash.py
@@ -2,7 +2,7 @@ import argparse
 from core.command_chain import CommandChain
 import config
 import logging
-import importlib
+import commands
 
 
 logger = logging.getLogger(__name__)
@@ -11,16 +11,15 @@ logger = logging.getLogger(__name__)
 def handle_exec(args:str) -> str:
     """callback for command execution"""
     
-    if args.c not in config.ALLOWED_COMMANDS:
-        logger.error(f"(error: 4001) - \"{args.c}\" Command is not allowed to be executed")
-        exit(4001)
-        
-    module = importlib.import_module("commands")
-    func = getattr(module, args.c)
-
-    chain = CommandChain([
-        func
-    ])
+    chain = CommandChain()
+    
+    for c in args.c:    
+        if c not in config.ALLOWED_COMMANDS:
+            logger.error(f"(error: 4001) - \"{c}\" Command is not allowed to be executed")
+            exit(4001)
+            
+        func = getattr(commands, c)
+        chain.add(func)
     
     output = chain.execute()
     print(output)
@@ -31,7 +30,7 @@ parser = argparse.ArgumentParser(
     description='linux cli tool for servers',
     epilog='see GNU license for more details'
 )
-parser.add_argument('-c', required=True)
+parser.add_argument('-c', required=True, action='append')
 parser.set_defaults(func=handle_exec)
 args = parser.parse_args()
 args.func(args)
